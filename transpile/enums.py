40485a6fa2b911320e3a70e6a0450ba8f2c7f029
Aaron Perl <perla@rpi.edu>
2016-11-02 02:31:24 -0400

    Organized code and improved polymorphism
    Changed static variables to instance variables
    Created enums.py file for project enums and created PinValue enum
    Created defaults.py for project default values and created BlockDefaults class
    Changed type to _type and created a type property that exposes the value
    of type

---
+++ transpile/enums.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class PinValue(Enum):
+    """An enum that represents the logical states that a digital pin can be in"""
+    low = 0
+    high = 1
+
+
+if __name__ == "__main__":
+    print("WARNING: This file is not meant to stand alone. Run main.py instead.")
+    exit(1)
